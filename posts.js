6485b4df5428ab500298ec678af445b3b3795fcc
Karysson <cr.karysson@gmail.com>
2025-05-05 16:58:18 -0300

    adicionei o slugfy - ainda não funciona o slug.

--- posts.js
+++ posts.js
@@ -1,13 +1,17 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var PostSchema = new Schema({
+const mongoose = require('mongoose');
+const slugify = require('slugify');
+
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
     titulo: {
         type: String,
         required: true
     },
     slug: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     conteudo: {
         type: String,
@@ -24,10 +28,27 @@ var PostSchema = new Schema({
     createdAt: {
         type: Date,
         default: Date.now
+    },
+    autor: {
+        type: String,
+        required: true
+    },
+    views: {
+        type: Number,
+        default: 0
     }
-}, {
-    collection: 'posts'
 });
 
-var Post = mongoose.model('Post', PostSchema);
-module.exports = Post;
+// Middleware para gerar slug antes de salvar
+PostSchema.pre('validate', function (next) {
+    if (!this.slug && this.titulo) {
+        this.slug = slugify(this.titulo, {
+            lower: true,
+            strict: true, 
+            locale: 'pt'  
+        });
+    }
+    next();
+});
+
+module.exports = mongoose.model('Post', PostSchema);
